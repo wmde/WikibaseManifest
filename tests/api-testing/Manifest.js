ee3a9756a9a72751272fb643c4169287fdc97b0a
addshore <adamshorland@gmail.com>
2020-04-17 12:02:45 +0100

    eslint, fix errors

--- tests/api-testing/Manifest.js
+++ tests/api-testing/Manifest.js
@@ -8,14 +8,14 @@ describe( 'Manifest', () => {
 			const { body } = await client.get( '/manifest' );
 			assert.hasAllKeys(
 				body,
-				[ 'name', 'rootScriptUrl', 'equivEntities', 'localRdfNamespaces', 'externalServices' , 'entities' ]
+				[ 'name', 'rootScriptUrl', 'equivEntities', 'localRdfNamespaces', 'externalServices', 'entities' ]
 			);
-			assert.typeOf( body.name, 'string' )
-			assert.typeOf( body.rootScriptUrl, 'string' )
-			assert.typeOf( body.equivEntities, 'object' )
-			assert.typeOf( body.localRdfNamespaces, 'object' )
-			assert.typeOf( body.externalServices, 'object' )
-			assert.typeOf( body.entities, 'object' )
+			assert.typeOf( body.name, 'string' );
+			assert.typeOf( body.rootScriptUrl, 'string' );
+			assert.typeOf( body.equivEntities, 'object' );
+			assert.typeOf( body.localRdfNamespaces, 'object' );
+			assert.typeOf( body.externalServices, 'object' );
+			assert.typeOf( body.entities, 'object' );
 		} );
 	} );
 } );
